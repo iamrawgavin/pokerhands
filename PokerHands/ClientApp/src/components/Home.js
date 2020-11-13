44dbe756091eba9f22725d453a6a06e6f63145e9
iamrawgavin <i@rawgav.in>
2020-11-12 22:30:59 -0800

    final commit prior to delivery

--- PokerHands/ClientApp/src/components/Home.js
+++ PokerHands/ClientApp/src/components/Home.js
@@ -6,20 +6,22 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <h1>Hello, iTrellis Recruiting!</h1>
+            <p>
+                The project I selected was Poker Hands. The solution I created has 3 projects within it.
+                <ul>
+                    <li>PokerEngine - class library that handles things like dealing a hand and determining winner of game</li>
+                    <li>PokerEngineTests - suite of tests to exercise the core logic of the application</li>
+                    <li>PokerHands - the web facing portion of the project (created by selecting new project > React.js template</li>
+                </ul>
+                This code was developed on a Mac with the community edition of Visual Studio 2019.
+            </p>
+            <p>
+                Please note, the <b>React</b> portion of the application is incomplete. It has been a while since I worked with it and did not want to spend too much time trying to thread the needle.
+                Basically, I ran into issues related to rendering the contents of the API call. Using the React Developer Tools in Firefox, I was able to confirm that the call was successful,
+                and I also display the winner of the game which was just a root-level string. I need to review how to render the child objects and child arrays, too. The API works fine, and assuming you
+                have not edited the launchSettings.json file and are running locally can be called <a target="_blank" href="https://localhost:32330/api/pokerengine">here</a>. It runs a new game each GET request.
+            </p>
       </div>
     );
   }
---
+++ PokerHands/ClientApp/src/components/Poker.js
@@ -0,0 +1,28 @@
+﻿import React, { Component } from 'react';
+
+export class Poker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {pokergame: {}};
+    }
+
+    componentDidMount() {
+        this.getPokerGame();
+    }
+
+    render() {
+        return (
+        <div>
+
+        <h1>Welcome to the Poker Game</h1>
+            Winner was {this.state.pokergame.Winner}
+            </div>
+            )
+    }
+
+    async getPokerGame() {
+        const response = await fetch('api/pokerengine');
+        const data = await response.json();
+        this.setState({pokergame: data});
+    }
+}
