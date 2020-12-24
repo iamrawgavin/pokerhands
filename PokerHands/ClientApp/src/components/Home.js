ac5f5d31d3f0174900b1706f5c70e141fdbe46ae
Gavin Donoho <github@whobought.me>
2020-12-24 08:34:28 -0800

    Update Home.js
    Fixed code so now it runs a hand and displays the data albeit in an ugly manner!

--- PokerHands/ClientApp/src/components/Home.js
+++ PokerHands/ClientApp/src/components/Home.js
@@ -16,12 +16,6 @@ export class Home extends Component {
                 </ul>
                 This code was developed on a Mac with the community edition of Visual Studio 2019.
             </p>
-            <p>
-                Please note, the <b>React</b> portion of the application is incomplete. It has been a while since I worked with it and did not want to spend too much time trying to thread the needle.
-                Basically, I ran into issues related to rendering the contents of the API call. Using the React Developer Tools in Firefox, I was able to confirm that the call was successful,
-                and I also display the winner of the game which was just a root-level string. I need to review how to render the child objects and child arrays, too. The API works fine, and assuming you
-                have not edited the launchSettings.json file and are running locally can be called <a target="_blank" href="https://localhost:32330/api/pokerengine">here</a>. It runs a new game each GET request.
-            </p>
       </div>
     );
   }
